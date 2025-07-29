aed4632cdae5f4df298f355776a8ab62248de38a
Cherelle11 <sifisosibiya012@gmail.com>
2025-07-29 15:16:42 +0200

    Add files via upload
    Project Structure

---
+++ smartroutes/src/test/java/com/fmtali/travelrisk/smartroutes/SmartroutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fmtali.travelrisk.smartroutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartroutesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
